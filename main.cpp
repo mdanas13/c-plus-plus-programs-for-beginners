d3b5cc9a6d033a43529aa3de74d50ece374d2750
mdanas13 <47151696+mdanas13@users.noreply.github.com>
2019-02-09 03:51:06 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* Addition of Two Numbers */
+
+using namespace std;
+
+int main()
+{
+   int n1, n2, sum;
+   cout<<" Enter Two Numbers\n";
+   cin>>n1>>n2;
+   sum=n1+n2;
+   cout<<"Sum is "<<sum<<"";
+
+
+   return 0;
+}
